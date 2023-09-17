eb8362a499de7520b1fb34e229b743744d285662
Sai Srinivas S S <saisrinivas2511@gmail.com>
2023-09-17 14:52:39 +0530

    Created a new project with basic configurations

---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./navbar.css";
+import logo from "../../assets/logo.png";
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="" />
+      <div className=""></div>
+    </nav>
+  );
+};
+
+export default Navbar;
